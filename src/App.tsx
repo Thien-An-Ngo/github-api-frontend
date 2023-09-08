980795fcebbeea9781af3ace6f9cbd18574232da
Thien-An Ngo <hurzely@Thien-Ans-MacBook-Pro.local>
2023-09-08 23:17:55 +0200

    finished task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
 import * as React from 'react'
+import { getUserData, IUserData } from './apiFunctions'
 
 export const App = (): JSX.Element => {
+    const [username, setUsername] = React.useState<string>('')
+    const [userData, setUserData] = React.useState<IUserData>()
+    const [isNoUser, setIsNoUser] = React.useState<boolean>(false)
+
+    const handleInputChange = event => {
+        const { value } = event.target
+        setUsername(value)
+    }
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        isNoUser && setIsNoUser(false)
+        setUserData(undefined)
+        const data = await getUserData(username)
+        if (data === "Error 404") {
+            setIsNoUser(true)
+            return
+        }
+        setUserData(data)
+    }
 
     return (
         <>
-            <h1>Hallo Victoria!</h1>
-        </>        
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    placeholder="enter github username"
+                    value={ username }
+                    onChange={handleInputChange}
+                />
+                <button
+                    type="submit"
+                >Search User</button>
+            </form>
+            {
+                isNoUser &&
+                <h3 color='red'>User Not Found</h3>
+            }
+            {
+                userData &&
+                <>
+                    <h1>{userData.username}</h1>
+                    <h3>Public Repositories: {userData.publicRepos}</h3>
+                    <hr />
+                    <h2>Repositories</h2>
+                    <table>
+                        <tbody>
+                            {userData.repoData.map(repo => <tr>
+                                <th>{repo.name}</th>
+                                <td>{repo.desc}</td>
+                            </tr>)}
+                        </tbody>
+                    </table>
+                    
+                </>
+            }
+        </>
     )
 }
---
+++ src/apiFunctions.ts
@@ -0,0 +1,68 @@
+import { Octokit } from 'octokit'
+
+export interface IUserData {
+    username: string,
+    publicRepos: number,
+    repoData: {name: string, desc: string}[]
+}
+
+const fetchData = async (method: string, path: string, data: any): Promise<any> => {
+    const octokit = new Octokit({auth: process.env.REACT_APP_GITHUB_TOKEN})
+
+    return await octokit.request(`${method} ${path}`, data)
+}
+
+// For scalability as there might be a need to get more user information later ons
+const getUser = async (username: string): Promise<
+    {publicRepos: number, reposUrl: string} | "Error 404"
+    > => {
+    const path = `/users/{username}`
+    const data = {
+        username: username,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    
+    const res:any = await fetchData('GET', path, data)
+                        .then(res => res)
+                        .catch(err => "Error 404")
+
+    if (res === "Error 404") {
+        return "Error 404"
+    }
+
+    console.log(res)
+    
+    return {
+        publicRepos: res.data.public_repos,
+        reposUrl: res.data.repos_url
+    }
+}
+
+export const getUserData = async (username: string): Promise<IUserData | "Error 404"> => {
+    const userInfo = await getUser(username)
+
+    if (userInfo === "Error 404") {
+        return "Error 404"
+    }
+
+    const {publicRepos, reposUrl} = userInfo
+
+    const repos = await fetchData(
+        'GET', '/users/{username}/repos', {
+            username: username,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        }
+        )
+
+    return {
+        username: username,
+        publicRepos: publicRepos,
+        repoData: repos.data.map(repo => {
+            return {name: repo.name, desc: repo.description}
+        })
+    }
+}
